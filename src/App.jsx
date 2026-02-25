e7b2a8210347f4601fae3ec68e0f2f851001d8da
kamlesh694 <kamleshpanwar3500@gmail.com>
2026-02-26 00:31:07 +0530

    About section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/common/Header'
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/common/Header"
+// import Home from "./components/pages/Home"
+
+// import Contact from "./components/pages/Contact"
+import About from "./components/pages/About"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Header />
-     
+    <Header/>
+    <div className="pt-[130px]">
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/about" element={<About/>} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </div>
     </>
   )
 }
